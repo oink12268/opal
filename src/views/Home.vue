3f2b44cb65a40474eff3e769fabbb4e0af57979d
SUHWAN KIM <oink12268@gmail.com>
2021-12-26 11:09:18 +0900

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="say">say hello</button>
   </div>
 </template>
 
@@ -11,8 +10,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+
   }
+
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    devServer: {
+        historyApiFallback: true,
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080'
+            }
+        },
+        hot: true,
+        overlay: true
+    },
+    transpileDependencies: [],
+    configureWebpack: {
+        plugins: []
+    }
+}
